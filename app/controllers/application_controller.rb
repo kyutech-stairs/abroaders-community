fc2088ba4b2fb9ca1a5cda6606f527a77181ce90
Nurin Izzati Jafri <nurinizzatijafri@gmail.com>
2019-06-19 23:31:06 +0900

    create static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    
+    protect_from_forgery with: :exception
+
+    def hello
+      render html: "hello, world! Welcome to Abrry!"
+    end
+
 end
