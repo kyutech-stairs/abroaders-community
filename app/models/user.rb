fa2201190cb5261f483c559b39c2a86603be1a99
Nurin Izzati Jafri <nurinizzatijafri@gmail.com>
2019-06-20 22:15:59 +0900

    passwordssss

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     # validates :sex, presence: true, length: { maximum: 10}
     # validates :address, presence: true, length: { maximum: 10}
     # validates :belonged_uni, presence: true, length: { maximum: 50}
-    has_secure_password
+    has_secure_passwordssss
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX }, uniqueness: true
