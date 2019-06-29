494eb9c0f96dfb971ba25e495d44b068c9194d8e
Nurin Izzati Jafri <nurinizzatijafri@gmail.com>
2019-06-29 19:32:19 +0900

    unrevised login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
+  #before_action :authenticate_user!, :only => [:show]
   def index
     @users=User.all
   end
   def show
     @user = User.find(params[:id])
-    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @currentUserEntry=Entry.where(user_id: current_user&.id)
     @userEntry=Entry.where(user_id: @user.id)
-    if @user.id == current_user.id
+    if @user.id == current_user&.id
     else
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'sessions/new'
-  # root 'static_pages#home'
-  # get 'static_pages/home',    to: 'static_pages#home'
-  get 'static_pages/help',    to: 'static_pages#help'
-  get  'static_pages/about',    to: 'static_pages#about'
-  post '/signup',  to: 'users#new'
-  get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'users#index'
-  delete '/logout', to:'sessions#destroy'
-  # resources :users
-  resources :users, :only => [:index, :show]
-  root "users#index"
+  resources :users, :only => [:index, :show, :create]
   resources :messages, :only => [:create]
   resources :rooms, :only => [:create, :show, :index]
+  #get 'users/index'
+  #et 'users/show'
+  #get 'sessions/new'
+  root 'static_pages#home'
+  # get 'static_pages/home',    to: 'static_pages#home'
+  #root "users#index"
+  get '/help',    to: 'static_pages#help'
+  get  '/about',    to: 'static_pages#about'
+  
+  #post '/signup',  to: 'users#new'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to:'sessions#destroy'
+  get '/logout', to: 'static_pages#home'
+  # resources :users
+  
 end
