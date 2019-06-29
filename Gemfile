b1b58cddb1ba2c6419415aa64bd5c22ece873261
Nurin Izzati Jafri <nurinizzatijafri@gmail.com>
2019-06-29 20:27:25 +0900

    solved errors at signup and login

--- Gemfile
+++ Gemfile
@@ -62,3 +62,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bcrypt'
+
+gem 'jquery-rails'
+gem 'bootstrap-sass'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to ABRRY for students!"
       redirect_to @user # Handle a successful save.
     else
