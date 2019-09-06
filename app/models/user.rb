31145246e56d4783dc3fbbbfdd5079327f30f124
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-09-06 13:56:20 +0900

    def user_params

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   # 入力されたものがemailとしてふさわしいかどうかを検証する正規表現
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },　format: { with: VALID_EMAIL_REGEX },　uniqueness: { case_sensitive: false }
+  validates :email, {presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }}
 
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:show, :create, :update, :destroy]
 end
