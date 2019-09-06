915defe079c7fad5897f911d77ad0b069e210126
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-09-07 03:32:29 +0900

    seed some sample users

---
+++ app/models/stay_type.rb
@@ -0,0 +1,2 @@
+class StayType < ApplicationRecord
+end
--- db/migrate/20190906165334_create_posts.rb
+++ db/migrate/20190906165334_create_posts.rb
@@ -1,6 +1,17 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.string :name
+      t.string :term
+      t.string :image
+      t.string :budget
+
+      t.text :comment
+
+      t.references :stay_type
+      t.references :country
+      t.references :language
+      t.references :user
 
       t.timestamps
     end
